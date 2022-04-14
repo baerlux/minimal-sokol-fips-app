83f57925955a1dfdadbbceb2133903dfed2448f8
baerlux <baerlux@pm.me>
2022-04-14 16:37:20 +0200

    update to triangle rendering with simple shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,48 @@
+#include <stdio.h>
+
 #include <sokol_app.h>
 #include <sokol_args.h>
 #include <sokol_gfx.h>
 #include <sokol_glue.h>
 
-#include <stdio.h>
+#include "simple.glsl.h"
 
-sg_pass_action pass_action = {};
+static struct {
+  sg_pipeline pip{};
+  sg_bindings bind{};
+  sg_pass_action pass_action{};
+} state;
 
 void init() {
   sg_setup({.context = sapp_sgcontext()});
-  pass_action.colors[0] = {.action = SG_ACTION_CLEAR,
-                           .value = {1.0f, 0.0f, 0.0f, 1.0f}};
+
+  float vertices[] = {// positions        //colors
+                      0.0f,  0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 1.0f,
+                      0.5f,  -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f,
+                      -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f};
+
+  state.bind.vertex_buffers[0] = sg_make_buffer(
+      {.data = SG_RANGE(vertices), .label = "triangle-vertices"});
+
+  sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend()));
+
+  sg_pipeline_desc pip_desc = {};
+  pip_desc.shader = shd;
+  pip_desc.layout.attrs[ATTR_vs_position] = {.format = SG_VERTEXFORMAT_FLOAT3};
+  pip_desc.layout.attrs[ATTR_vs_color0] = {.format = SG_VERTEXFORMAT_FLOAT4};
+  pip_desc.label = "triangle-pipeline";
+
+  state.pip = sg_make_pipeline(pip_desc);
+
+  state.pass_action.colors[0] = {.action = SG_ACTION_CLEAR,
+                                 .value = {0.0f, 0.0f, 0.0f, 1.0f}};
 }
 
 void frame() {
-  float g = pass_action.colors[0].value.g + 0.01f;
-  pass_action.colors[0].value.g = (g > 1.0f) ? 0.0f : g;
-  sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+  sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
+  sg_apply_pipeline(state.pip);
+  sg_apply_bindings(&state.bind);
+  sg_draw(0, 3, 1);
   sg_end_pass();
   sg_commit();
 }
